8538b533123dd65d0581cc9bd106581436040015
Romain Díaz de León <105795204+Nesscafebaby@users.noreply.github.com>
2025-10-25 23:33:14 -0600

    Add files via upload

---
+++ backEstudiantes/routes/auth.routes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const { login } = require('../controllers/auth.controller');
+
+const router = express.Router();
+//POST /api/login
+router.post('/login', login);
+module.exports = router;
